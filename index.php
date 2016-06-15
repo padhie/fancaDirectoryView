05326cb8968d5086ca74611af547e49b9c542f59
padhie <padhiez@hotmail.de>
2016-06-15 22:37:37 +0200

    new feature
    - navi
    - aktuelles verzeichnis
    - unterverzeichnisse

--- index.php
+++ index.php
@@ -1,13 +1,48 @@
 <?php
-	$sTemplate = "<div class='container'><p><a href='{NAME}'>{NAME}</a></p>{SUBDIR}</div>";
-
+	$sTemplate 		= "<div class='container'><p><a href='?dir={DIR}/{NAME}'>{NAME}</a></p>{SUBS}</div>";
+	$sSubTemplate	= " - <a href='{DIR}{SUB}'>{SUB}</a><br />";
+	$sCurrend = __DIR__;
+	if (isset($_GET["dir"]) && !empty($_GET["dir"])) {
+		$sCurrend .= "/".$_GET["dir"];
+	}
+	
+	function getSubfolderTemplate($sDir) {
+		global $sSubTemplate;
+		
+		$sOutput = "";
+		foreach (scandir($sDir) AS $sSubdir) {	
+			if ($sSubdir !== "." && $sSubdir !== "..") {
+				if (is_dir($sSubdir)) {
+					$sSubdir = "/".$sSubdir;
+				}
+				$_sTpl		= str_replace("{DIR}", $sDir, $sSubTemplate);
+				$sOutput	.= str_replace("{SUB}", $sSubdir, $_sTpl);
+			}
+		}
+		return $sOutput;
+	}
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>fancaDirectoryView</title>
 	<style type="text/css">
+		body {
+			margin: 0;
+		}
+		nav {
+			background-color: #E1E1E1;
+    		margin: 0;
+    		min-height: 30px;
+    		padding: 10px 60px 0;
+		}
+		nav #current {
+			float: left;
+		}
+		nav #back {
+			float: right;
+		}		
 		div.container {
 			width: 20%;
 			margin-left: 3%;
@@ -25,19 +60,27 @@
 	</style>
 </head>
 <body>
-	<?php foreach (scandir(__DIR__) AS $sDir) : ?>
-		<?php if ($sDir !== "." && $sDir !== "..") : ?>
-			<?php $_sSubdir = ""; ?>
-			<?php if (is_dir(__DIR__."/".$sDir)) : ?>
-				<?php foreach (scandir(__DIR__."/".$sDir) AS $sSubdir) : ?>
-					<?php if ($sSubdir !== "." && $sSubdir !== "..") : ?>
-						<?php $_sSubdir = " - <a href='".$sDir."/".$sSubdir."'>".$sSubdir."</a><br />"; ?>
-					<?php endif; ?>
-				<?php endforeach; ?>
+	<nav>
+		<div id="current">Current: <?php echo str_replace(__DIR__."/", "", $sCurrend); ?></div>
+		<div id="back"><a href="#" onClick="window.history.back();"><<<</a></div>
+	</nav>
 
+	<?php // current directory
+		$_sTmpOutput = str_replace("{DIR}", "", $sTemplate);
+		$_sTmpOutput = str_replace("{NAME}", "", $_sTmpOutput);
+		$_sTmpOutput = str_replace("{SUBS}", getSubfolderTemplate($sCurrend), $_sTmpOutput);
+		echo $_sTmpOutput;
+	?>
+
+	<?php // Subfolder ?>
+	<?php foreach (scandir($sCurrend) AS $sDir) : ?>
+		<?php if (strpos($sDir, ".") === false || strpos($sDir, ".") >= 1) : ?>
+			<?php if (is_dir($sCurrend."/".$sDir)) : ?>
+				<?php $_sSubdir = getSubfolderTemplate($sCurrend."/".$sDir); ?>
 				<?php
-					$_sTmpOutput = str_replace("{NAME}", $sDir, $sTemplate);
-					$_sTmpOutput = str_replace("{SUBDIR}", $_sSubdir, $_sTmpOutput);
+					$_sTmpOutput = str_replace("{DIR}", str_replace(__DIR__, "", $sCurrend), $sTemplate);
+					$_sTmpOutput = str_replace("{NAME}", $sDir, $_sTmpOutput);
+					$_sTmpOutput = str_replace("{SUBS}", $_sSubdir, $_sTmpOutput);
 					echo $_sTmpOutput;
 				?>
 			<?php endif; ?>
